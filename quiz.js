7b4706e6e738580008016554da942bb57de0eec9
candybez251 <kalkidanblemma@gmail.com>
2024-07-21 00:52:28 +0300

    added all filess

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function checkAnswer() {
+    correctAnswer = '4'; 
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value; 
+    let feedback = document.querySelector('#feedback');
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = 'Correct! Well done.';
+    } else{
+        feedback.textContent = "That's incorrect. Try again!";
+    };
+}
+const submitAnswer = document.getElementById('submit-answer');
+
+submitAnswer.addEventListener('click', checkAnswer); 
+
